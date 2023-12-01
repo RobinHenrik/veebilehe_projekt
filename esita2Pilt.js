0ec4b50a14cdcf44d0d338061118efae7d567585
RobinHenrik <robin.neem@gmail.com>
2023-12-01 23:28:09 +0200

    Viimane retsept. Kõikide failide validaatorist läbilaskmine ja parandamine

--- esita2Pilt.js
+++ esita2Pilt.js
@@ -14,7 +14,7 @@ function esitaPildid(){ //sama, mis esitaPilt.js kuid pildivahetus ajastus on 2.
         document.getElementById("isa2pilt").style.backgroundImage="url(hakklihakaste.jpg)";
     }, 17500)
     setTimeout(function() {
-        document.getElementById("isa2pilt").style.backgroundImage="url(koduJuust.jpeg)";
+        document.getElementById("isa2pilt").style.backgroundImage="url(kanakarri.jpg)";
     }, 22500)
 }
 esitaPildid();
